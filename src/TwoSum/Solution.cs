68a3d4325e884a97afd6f909aa63e810c8eb57ab
zhaoming <zhaoming@mallcoo.cn>
2017-04-12 21:24:55 +0800

    Two Sum Solution

---
+++ src/TwoSum/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[] { -1 }; // -1表示未找到
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        result = new int[] { i, j };
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/algorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            Console.WriteLine(string.Join(",", TwoSum.Solution.TwoSum(nums, 8)));
+
+            Console.ReadLine();
+        }
+    }
+}
